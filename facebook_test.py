beeff5b550b1b13d6706b3cfb0ebab02582136c1
Sam Radhakrishnan <sk09idm@gmail.com>
2015-04-26 18:48:02 +0530

    Added downloading friends images from facebook

--- facebook_test.py
+++ facebook_test.py
@@ -1,8 +1,20 @@
-import fb, json, urllib2
+import fb, json, urllib
 from config import token
-print token
+
+#print token
 facebook=fb.graph.api(token)
-userNode =facebook.get_object(cat="single", id="me", fields=['friends', 'name', 'email', 'likes', 'education'] )
-print  userNode
+userNode =facebook.get_object(cat="single", id="me", fields=['friends'] )
+#print userNode
+count = 0
+for f in userNode['friends']['data']:
+  name = f['name']
+  pid = f['id']
+  p_url = "https://graph.facebook.com/"+pid+"/picture"
+  print p_url
+  count+=1
+  img = open('images/'+name+"_"+pid+".jpg", "wb")
+  img.write(urllib.urlopen(p_url).read())
+  img.close()
+  print "Profile pic of " + name + " downloaded "
 
-
+print count
